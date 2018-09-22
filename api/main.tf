c58b113faf68b3a7a69050fa388a255d02f9be8a
Lucas Hanke <hankelucas@gmail.com>
2018-09-22 16:29:22 -0300

    add custom domain name for api gateway

--- api/main.tf
+++ api/main.tf
@@ -33,6 +33,24 @@ resource "aws_api_gateway_rest_api" "eutambem_api" {
   description = "API for Eu Tambem - ${terraform.workspace}"
 }
 
+resource "aws_api_gateway_domain_name" "api_domain_name" {
+  domain_name = "api-${terraform.workspace}.eutambem.org"
+
+  certificate_arn = "${data.terraform_remote_state.common.certificate_arn}"
+}
+
+resource "aws_route53_record" "api_record" {
+  zone_id = "${data.terraform_remote_state.common.zone_id}"
+  name = "${aws_api_gateway_domain_name.api_domain_name.domain_name}"
+  type = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.api_domain_name.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.api_domain_name.cloudfront_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
 module "lambda" {
   source           = "./lambda"
   api_gw_id        = "${aws_api_gateway_rest_api.eutambem_api.id}"
