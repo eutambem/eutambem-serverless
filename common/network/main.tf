a0c0742f5185886e4eb0340ec75cb449d98d1422
Lucas Hanke <hankelucas@gmail.com>
2018-09-26 19:18:33 -0300

    fix cidr block for subnet c

--- common/network/main.tf
+++ common/network/main.tf
@@ -53,7 +53,7 @@ resource "aws_subnet" "subnet_b" {
 
 resource "aws_subnet" "subnet_c" {
   vpc_id                  = "${aws_vpc.vpc.id}"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.region}c"
 
